2b3052a6819fc98aa63b805fdcbc23963085234e
Sutantra-ArunC <53487986+sutantra-2019@users.noreply.github.com>
2020-11-25 13:52:03 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket"
+    bucket = "sutantra-terraform-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
